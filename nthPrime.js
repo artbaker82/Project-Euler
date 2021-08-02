1a5270d0b6f95a100030386269bd58018c49da37
Arthur Baker <artbaker82@gmail.com>
2021-08-02 09:44:47 -0600

    largest product in a series

--- nthPrime.js
+++ nthPrime.js
@@ -17,28 +17,61 @@
 //   //return primes.map((x, index) => (x ? index : null)).filter((x) => x);
 // };
 
-const nthPrime = (num) => {
-  //use sieve of eratosthenes to generate 15 times more primes than n
-  let sieveRange = num * 15;
-  console.log(sieveRange);
-  console.log(num);
-  let primes = Array(sieveRange + 1).fill(true);
-  primes[0] = false;
-  primes[1] = false;
+// const nthPrime = (num) => {
+//   //use sieve of eratosthenes to generate 15 times more primes than n
+//   let sieveRange = num * 15;
+//   console.log(sieveRange);
+//   console.log(num);
+//   let primes = Array(sieveRange + 1).fill(true);
+//   primes[0] = false;
+//   primes[1] = false;
 
-  for (let i = 2; i <= Math.sqrt(sieveRange); i++) {
-    if (primes[i]) {
-      for (let j = i * 2; j <= sieveRange; j += i) {
-        primes[j] = false;
+//   for (let i = 2; i <= Math.sqrt(sieveRange); i++) {
+//     if (primes[i]) {
+//       for (let j = i * 2; j <= sieveRange; j += i) {
+//         primes[j] = false;
+//       }
+//     }
+//   }
+//   // convert to numbers, filter out non primes, return index n -1, since that would be the nth prime
+//   let primeNums = primes
+//     .map((val, idx) => {
+//       return val ? idx : null;
+//     })
+//     .filter((x) => x)[num - 1];
+//   console.log(primeNums);
+// };
+// nthPrime(10001);
+
+///freecodecamp solution
+
+function nthPrime(n) {
+  const primes = [2];
+
+  let num = 3,
+    isPrime = true;
+
+  //loop until primesarray equals n
+
+  while (primes.length < n) {
+    //all the prime numbers less than n are always <= square root of n
+    let max = Math.ceil(Math.sqrt(num));
+    for (let i = 0; primes[i] <= max; i++) {
+      //if num is divisible by any of the primes less than the square root, num is composite
+      if (num % primes[i] == 0) {
+        isPrime = false;
+        break;
       }
     }
+
+    if (isPrime) primes.push(num);
+    isPrime = true;
+
+    //An optimization technique, since we know of all even numbers only 2 is a prime number, we can skip the rest
+    num += 2;
   }
-  // convert to numbers, filter out non primes, return index n -1, since that would be the nth prime
-  let primeNums = primes
-    .map((val, idx) => {
-      return val ? idx : null;
-    })
-    .filter((x) => x)[num - 1];
-  console.log(primeNums);
-};
-nthPrime(10001);
+  //return the last number
+  return primes[primes.length - 1];
+}
+
+console.log(nthPrime(100));
